0afb9e855bd15883dd196bef38426902a279cb60
Vdor01 <gombosvidor@gmail.com>
2025-10-02 18:07:39 +0200

    Fixed bug where empty courses can be made

--- src/components/subjects/Subject.jsx
+++ src/components/subjects/Subject.jsx
@@ -49,11 +49,29 @@ const Subject = ({ subject }) => {
         const form = document.getElementById("course_form_" + subject.code);
         const formData = new FormData(form);
 
-        const error = formData.get('code') === "0" || formData.get('code') === "-1";
-        const codeInput = document.querySelector(`#course_form_${subject.code} input[name='code']`);
+        let emptyFields = false;
 
-        if (error) {
+        const dataNames = ['code', 'instructor', 'location', 'startTime', 'endTime'];
+        dataNames.forEach(name => {
+            const input = form.querySelector(`#course_form_${subject.code} input[name='${name}']`);
+            if (!input.value) {
+                input.setCustomValidity("Ennek a mezőnek a kitöltése kötelező!");
+                input.reportValidity();
+                emptyFields = true;
+            } else {
+                input.setCustomValidity("");
+            }
+        });
+
+        const codeInput = document.querySelector(`#course_form_${subject.code} input[name='code']`);
+        const codeInputValue = codeInput.value;
+        const codeError = codeInputValue === "0" || codeInputValue === "-1";
+
+        if (codeError) {
             codeInput.setCustomValidity("A kód nem lehet 0 vagy -1!");
+            codeInput.reportValidity();
+        } else if (emptyFields) {
+            // Do nothing, as the individual fields have already reported their validity
         } else {
             codeInput.setCustomValidity("");
             addCourse(
@@ -68,7 +86,6 @@ const Subject = ({ subject }) => {
                 formData.get('notes')
             );
         }
-        codeInput.reportValidity();
     }
 
     /**
@@ -168,7 +185,7 @@ const Subject = ({ subject }) => {
                                                 type="text"
                                                 name='instructor'
                                                 placeholder="Példa Béla"
-                                                className="w-full max-w-xs input input-bordered input-sm"
+                                                className="w-full max-w-xs input input-bordered input-sm invalid:border-error"
                                             />
                                         </th>
                                         <th className='w-2/12'>
@@ -176,7 +193,7 @@ const Subject = ({ subject }) => {
                                                 type="text"
                                                 name='location'
                                                 placeholder="Északi Tömb 7.15 (PC11)"
-                                                className="w-full max-w-xs input input-bordered input-sm"
+                                                className="w-full max-w-xs input input-bordered input-sm invalid:border-error"
                                             />
                                         </th>
                                         <th className='w-2/12'>
@@ -193,13 +210,13 @@ const Subject = ({ subject }) => {
                                             <input
                                                 type="time"
                                                 name='startTime'
-                                                className="input input-bordered input-sm"
+                                                className="input input-bordered input-sm invalid:border-error"
                                             />
                                             -
                                             <input
                                                 type="time"
                                                 name='endTime'
-                                                className="input input-bordered input-sm"
+                                                className="input input-bordered input-sm invalid:border-error"
                                             />
                                         </th>
                                         <th className='w-2/12'>
